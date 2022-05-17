80ba6fa078c3003c0853354f242c76661393c1b4
faelcardoso <faelcjeronymo@gmail.com>
2022-05-17 14:19:39 -0300

    Commit Inicial

---
+++ Seguradora_Pim/Tela_Opcoes_Funcionario.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Seguradora_Pim {
+    partial class Tela_Opcoes_Funcionario {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent() {
+            this.Cabecalho = new System.Windows.Forms.Panel();
+            this.Titulo_Cabecalho = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.Cabecalho.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Cabecalho
+            // 
+            this.Cabecalho.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Cabecalho.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(26)))), ((int)(((byte)(47)))), ((int)(((byte)(67)))));
+            this.Cabecalho.Controls.Add(this.Titulo_Cabecalho);
+            this.Cabecalho.Location = new System.Drawing.Point(0, 0);
+            this.Cabecalho.Name = "Cabecalho";
+            this.Cabecalho.Size = new System.Drawing.Size(1184, 72);
+            this.Cabecalho.TabIndex = 0;
+            // 
+            // Titulo_Cabecalho
+            // 
+            this.Titulo_Cabecalho.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.Titulo_Cabecalho.AutoSize = true;
+            this.Titulo_Cabecalho.BackColor = System.Drawing.Color.Transparent;
+            this.Titulo_Cabecalho.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Titulo_Cabecalho.ForeColor = System.Drawing.Color.White;
+            this.Titulo_Cabecalho.Location = new System.Drawing.Point(439, 25);
+            this.Titulo_Cabecalho.Name = "Titulo_Cabecalho";
+            this.Titulo_Cabecalho.Size = new System.Drawing.Size(307, 23);
+            this.Titulo_Cabecalho.TabIndex = 1;
+            this.Titulo_Cabecalho.Text = "Sistema de Gerenciamento de Apólices";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 78);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1160, 609);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Tela_Opcoes_Funcionario
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1184, 699);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.Cabecalho);
+            this.Name = "Tela_Opcoes_Funcionario";
+            this.Text = "Sistema de Gerenciamento de Apólices";
+            this.Cabecalho.ResumeLayout(false);
+            this.Cabecalho.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel Cabecalho;
+        private System.Windows.Forms.Label Titulo_Cabecalho;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ Seguradora_Pim/Tela_Opcoes_Funcionario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Seguradora_Pim {
+    public partial class Tela_Opcoes_Funcionario : Form {
+        public Tela_Opcoes_Funcionario() {
+            InitializeComponent();
+        }
+    }
+}
